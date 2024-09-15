544ef8b3c8e0e5970f82b517f9b0495234ff21fe
Ayush Kumar Tripathi <ayushtripathi2003@gmail.com>
2024-09-16 01:14:38 +0530

    Updated cloudinary.config.js to handle errors during cloudinary configuration

--- server/config/cloudinary.config.js
+++ server/config/cloudinary.config.js
@@ -1,12 +1,15 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 const cloudinary_config = () => {
-  console.log(process.env.PORT);
-  cloudinary.config({
-    cloud_name: process.env.cloudinary_cloud_name,
-    api_key: process.env.cloudinary_api_key,
-    api_secret: process.env.cloudinary_api_secret
-  });
+  try {
+    cloudinary.config({
+      cloud_name: process.env.cloudinary_cloud_name,
+      api_key: process.env.cloudinary_api_key,
+      api_secret: process.env.cloudinary_api_secret
+    });
+  } catch (error) {
+    console.log("Error occurred for Cloudinary Configuration!!!",error);
+  }
 } 
 
 export default cloudinary_config;
